70644c8e38fbdf1cb6800e4c8bf90ffd5a16bc33
Eric Barreto <ericbarreto521@gmail.com>
2024-02-16 17:03:17 -0300

    Feat: Iniciando implementação do dijkstra, já montando quadro de distâncias

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ with open('facebook.txt', 'r') as arquivo:
 # criar o grafo 
 grafo = {}
 
+def dijkstra(grafo, vertice_inicial, vertice_final):
+    quadro_distancias = {v: float('inf') for v in  range(len(grafo))}
+    quadro_distancias[vertice_inicial] = 0
+    fila_visita = [(0, vertice_inicial)]
+
+    while fila_visita:
+        distancia_atual, vertice_atual = min(fila_visita)
+        fila_visita.remove(min(fila_visita))
+        for dados_vizinho in grafo[vertice_atual]:
+            vizinho, peso = dados_vizinho
+            peso_cumulativo = distancia_atual + peso
+            if peso_cumulativo < quadro_distancias[vizinho]:
+                quadro_distancias[vizinho] = peso_cumulativo
+                fila_visita.append((peso_cumulativo, vizinho))
+    print(quadro_distancias)
+
 # Iterar sobre as linhas do arquivo
 for linha in linhas:
     # Divindo em chave e valor
@@ -18,4 +34,13 @@ for linha in linhas:
     else:
         grafo[int(chave)] = [(int(valor), numero_aleatorio)]
 
-print(grafo)
+    # CRIA A RELAÇÃO AO CONTRÁRIO TAMBÉM, OU SEJA, GRAFO NÃO DIRECIONAL
+    if int(valor) in grafo:
+        grafo[int(valor)].append((int(chave), numero_aleatorio))
+    else:
+        grafo[int(valor)] = [(int(chave), numero_aleatorio)]
+
+
+# print(grafo)
+
+dijkstra(grafo, 0, 18)
